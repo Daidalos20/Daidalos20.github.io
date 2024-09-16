95027473ae57030d1c51c716d7a9aa4159f90bdd
Daidalos20 <64791332+Daidalos20@users.noreply.github.com>
2024-09-16 17:58:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,645 @@
+const item_containers = {
+  "RTW": [{
+    "name": "Defibrillator",
+    "location": "Fahrzeug 1, Fach A"
+  },
+          {
+            "name": "Sauerstoffflasche",
+            "location": "Fahrzeug 2, Fach B"
+          },
+          {
+            "name": "Erste-Hilfe-Kasten",
+            "location": "Wache 1, Regal 3"
+          },
+          {
+            "name": "Trage",
+            "location": "Fahrzeug 1, Fach C"
+          },
+          {
+            "name": "Feuerlöscher",
+            "location": "Fahrzeug 2, Fach D"
+          },
+          {
+            "name": "Medizinische Handschuhe",
+            "location": "Wache 1, Schrank 2"
+          },
+          {
+            "name": "Atemschutzmaske",
+            "location": "Fahrzeug 1, Fach E"
+          }
+         ],
+  "rucksack": [{
+    "name": "Erste-Hilfe-Set",
+    "location": "Fach 1"
+  },
+               {
+                 "name": "Verbandpäckchen",
+                 "location": "Fach 2"
+               },
+               {
+                 "name": "Schmerzmittel",
+                 "location": "Fach 3"
+               },
+               {
+                 "name": "Blutdruckmessgerät",
+                 "location": "Fach 4"
+               },
+               {
+                 "name": "Puls oximeter",
+                 "location": "Fach 5"
+               },
+               {
+                 "name": "Klebeband",
+                 "location": "Fach 6"
+               },
+               {
+                 "name": "Gaze",
+                 "location": "Fach 7"
+               },
+               {
+                 "name": "Desinfektionsmittel",
+                 "location": "Fach 8"
+               }
+              ],
+  "TSF-W": [
+  {
+    "name": "Moters\u00e4ge",
+    "location": "G3(mitte mitte);1"
+  },
+  {
+    "name": "Ersatzkette f\u00fcr Motors\u00e4ge",
+    "location": "G3(mitte mitte);2"
+  },
+  {
+    "name": "Schnittschutzhose",
+    "location": "GH(rechts neben Spinde);2"
+  },
+  {
+    "name": "Motors\u00e4genhelm",
+    "location": "G3(mitte mitte);1"
+  },
+  {
+    "name": "F\u00e4ll- und Spaltkeile",
+    "location": "G3(mitte mitte);4"
+  },
+  {
+    "name": "Doppelkanister mit Motors\u00e4genkraftstoff und Ketten\u00f6l",
+    "location": "G3(oben mitte);1"
+  },
+  {
+    "name": "Stromerzeuger",
+    "location": "G2(unten rechts);1"
+  },
+  {
+    "name": "Kanister f\u00fcr Stromerzeuger",
+    "location": "G2(unten links);1"
+  },
+  {
+    "name": "Leitungsroller 50m",
+    "location": "G2(mitte rechts);1"
+  },
+  {
+    "name": "Flutlichtstrahler",
+    "location": "G2(mitte mittte);2"
+  },
+  {
+    "name": "Stativ f\u00fcr Flutlichtstrahler",
+    "location": "G2(mitte mitte);1"
+  },
+  {
+    "name": "Schaumaufsatz C-Hohlstrahlrohr",
+    "location": "G1(unten links, zw. Halligan und Axt);1"
+  },
+  {
+    "name": "Zumischer Z2R",
+    "location": "G4(mitte leicht links, festeingebaut);1"
+  },
+  {
+    "name": "Ansaugschlauch D (f\u00fcr Zumischer)",
+    "location": "G4(mitte mitte, \u00fcber Schaumkanister);1"
+  },
+  {
+    "name": "Schaummittelbeh\u00e4lter",
+    "location": "G4(unten mitte);1"
+  },
+  {
+    "name": "Ansaugreduzierung f\u00fcr Zumischer",
+    "location": "G4(am Zumischer angesteckt);1"
+  },
+  {
+    "name": "Hohlsteiner Schaufel",
+    "location": "G1(oben mitte);1"
+  },
+  {
+    "name": "F\u00e4llheber",
+    "location": "G1(unten links)1;1"
+  },
+  {
+    "name": "Rauchvorhang",
+    "location": "G1(am l. Schlauchtragekorb);1"
+  },
+  {
+    "name": "Druckbel\u00fcfter",
+    "location": "G3(unten links);1"
+  },
+  {
+    "name": "Tauchpumpe",
+    "location": "G3(unten mitte);1"
+  },
+  {
+    "name": "Lichtmast",
+    "location": "auf dem Dach (Fernbedienung in GR)"
+  },
+  {
+    "name": "Euro-Blitz",
+    "location": "G4(unten mitte);2"
+  },
+  {
+    "name": "Absperrhahn C",
+    "location": "G4(mitte links, von unten angekuppelt);1"
+  },
+  {
+    "name": "Faltsignal",
+    "location": "G2(oben mitte);2"
+  },
+  {
+    "name": "Bockleiterteil Steckleitern",
+    "location": "G2(oben rechts);1"
+  },
+  {
+    "name": "Flatterbandroller",
+    "location": "G2(unten links);1"
+  },
+  {
+    "name": "Bandschlinge 2m",
+    "location": "G4 bei den PAs/GR bei den PAs;insg. 2"
+  },
+  {
+    "name": "W\u00e4rmebildkamera",
+    "location": "MR(R\u00fcckwand);1"
+  },
+  {
+    "name": "AED",
+    "location": "MR;1"
+  },
+  {
+    "name": "Pack Hygiene Handschuhe",
+    "location": "MR;1"
+  },
+  {
+    "name": "Wagenheber",
+    "location": "Bank;1"
+  },
+  {
+    "name": "L\u00f6schdecke",
+    "location": "MR;1"
+  },
+  {
+    "name": "Bauplane 2qm",
+    "location": "Bank;1"
+  },
+  {
+    "name": "Rohrzange",
+    "location": "Bank;1"
+  },
+  {
+    "name": "Druckluft \"Pistole\"",
+    "location": "Bank;1"
+  },
+  {
+    "name": "Corona Kit (Schutzbrille+Hygienehandschuhe)",
+    "location": "Bank;1"
+  },
+  {
+    "name": "GF Weste",
+    "location": "GF Sitz im FR;1"
+  },
+  {
+    "name": "EL Weste",
+    "location": "FR"
+  },
+  {
+    "name": "diverse Einsatzpl\u00e4ne/Karten",
+    "location": "Mittelfach FR"
+  },
+  {
+    "name": "Warnweste",
+    "location": "in den T\u00fcren im MR; 9"
+  },
+  {
+    "name": "PA",
+    "location": "MR, G4(unten rechts); insg. 4"
+  },
+  {
+    "name": "Atemschutzmaske",
+    "location": "MR, G4(unten rechts hinten); insg. 4"
+  },
+  {
+    "name": "ABEK Filter",
+    "location": "Bank;4"
+  },
+  {
+    "name": "Atemschutz\u00fcberwachung",
+    "location": "G4(unten rechts, rechts neben PAs);1"
+  },
+  {
+    "name": "Fluchthaube",
+    "location": "Bank;1"
+  },
+  {
+    "name": "K\u00fcbelspritze",
+    "location": "G3(unten rechts);1"
+  },
+  {
+    "name": "ABC Pulverl\u00f6scher",
+    "location": "G3(unten Rechts);1"
+  },
+  {
+    "name": "Druckschl\u00e4uche B 20m",
+    "location": "G4(oben mitte)+G4(unten links am Schnellangriffsverteiler);9+1=10"
+  },
+  {
+    "name": "5m B-Schlauch",
+    "location": "G4(oben ganz links);1"
+  },
+  {
+    "name": "Tragek\u00f6rbe mit C-Schlauch 15m",
+    "location": "G1(mitte rechts);3*3=9"
+  },
+  {
+    "name": "Schnellangriffschlauch C 15m + Hohlstrahlrohr",
+    "location": "G4(unten links);1"
+  },
+  {
+    "name": "Saugschlauch A",
+    "location": "G1(mitte links);4"
+  },
+  {
+    "name": "Saugkorb A",
+    "location": "G3(oben rechts);1"
+  },
+  {
+    "name": "Saugschutzkorb (Draht)",
+    "location": "G3(oben rechts);1"
+  },
+  {
+    "name": "Standrohr DIN",
+    "location": "G3(oben links);1"
+  },
+  {
+    "name": "Standrohr W\u00fcrtembergisch",
+    "location": "G4(oben rechts);1"
+  },
+  {
+    "name": "Sammelst\u00fcck A-2B",
+    "location": "G4(mitte links);1"
+  },
+  {
+    "name": "Verteiler",
+    "location": "G4(unten links);1"
+  },
+  {
+    "name": "BC-\u00dcbergangsst\u00fcck",
+    "location": "G4(mitte links, von unten angsbracht);2"
+  },
+  {
+    "name": "B-Mehrzweckstrahlrohr mit St\u00fczukr\u00fcmmer",
+    "location": "G4(mitte mitte);1"
+  },
+  {
+    "name": "Hohlstrahlrohr C",
+    "location": "G1(mitte mitte, von unten eingeh\u00e4ngt);3"
+  },
+  {
+    "name": "Mehrzweckleine (Rot)",
+    "location": "G3(oben rechts);1"
+  },
+  {
+    "name": "Seilschlauchhalter (Seilst\u00fccke)",
+    "location": "MR bei PA+ G4 bei PA; 1+2=3"
+  },
+  {
+    "name": "Schlauchbr\u00fccke",
+    "location": "G1(unten);3"
+  },
+  {
+    "name": "Kupplungsschl\u00fcssel ABC f\u00fcr ua. B-Schl\u00e4uche (Metall mit schwarzem Griff)",
+    "location": "GR(unten links);3"
+  },
+  {
+    "name": "Schl\u00fcssel B f\u00fcr \u00dcberfluhrhydrant (rot)",
+    "location": "G4(links neben/hinter PAs);1"
+  },
+  {
+    "name": "Schl\u00fcssel C f\u00fcr Unterflurhydrant Norm",
+    "location": "G3(oben links);1"
+  },
+  {
+    "name": "Schl\u00fcssel C f\u00fcr Unterflurhydrant W\u00fcrtembergisch",
+    "location": "G4(oben rechst);1"
+  },
+  {
+    "name": "Paar Schachthaken mit Kette",
+    "location": "G3(mitte rechts, von Rechts ans Blech befestigt, \u00fcberm Feuerl\u00f6scher);1"
+  },
+  {
+    "name": "Standrohrhalter",
+    "location": "G4(oben rechts);1"
+  },
+  {
+    "name": "Steckleiter 4Teilig + Einsteckteil",
+    "location": "GR(oben);1"
+  },
+  {
+    "name": "Feuerwehrleine im Tragebeutel",
+    "location": "MR, G4 (bei/hinter PAs); gefordert:4"
+  },
+  {
+    "name": "Notfallrucksack",
+    "location": "MR;1"
+  },
+  {
+    "name": "Schleifkorbtrage",
+    "location": "G3(oben mitte);1"
+  },
+  {
+    "name": "Wolldecke",
+    "location": "Bank;1"
+  },
+  {
+    "name": "Tragetuch mit Tasche",
+    "location": "Bank;1"
+  },
+  {
+    "name": "Taschenlampen",
+    "location": "MR+FR; 3+1?"
+  },
+  {
+    "name": "Warnleuchte StVZO",
+    "location": "MR;2"
+  },
+  {
+    "name": "Winkerkelle",
+    "location": "MR;2"
+  },
+  {
+    "name": "Pylonen",
+    "location": "G2(oben links);4"
+  },
+  {
+    "name": "Handspreckfunkger\u00e4te",
+    "location": "MR+FR;3+1"
+  },
+  {
+    "name": "Mulde Stahl",
+    "location": "G3(mitte rechts);1"
+  },
+  {
+    "name": "Tragkraftspritze PFPN 10-1000",
+    "location": "GR;1"
+  },
+  {
+    "name": "Werkzeugkasten",
+    "location": "G1(oben rechts);1"
+  },
+  {
+    "name": "Halligan-Tool",
+    "location": "G1(unten links unter den Saugschl\u00e4uchen);1"
+  },
+  {
+    "name": "Spalthammer",
+    "location": "G1(unten mitte);1"
+  },
+  {
+    "name": "Feuerwehraxt (mit Spitze auf Gegenseite)",
+    "location": "G1(unten mitte);1"
+  },
+  {
+    "name": "Holzaxt (flat axe)",
+    "location": "G1(unten mitte);1"
+  },
+  {
+    "name": "B\u00fcgels\u00e4ge",
+    "location": "G1(unten links unter Saugschl\u00e4uche);1"
+  },
+  {
+    "name": "Bolzenschneider",
+    "location": "G1(mitte mitte, links von Strahlrohren);1"
+  },
+  {
+    "name": "Stechschaufel",
+    "location": "G1(oben mitte);1"
+  },
+  {
+    "name": "Sto\u00dfbesen",
+    "location": "G1(oben mitte);2"
+  },
+  {
+    "name": "Unterlegkeil",
+    "location": "G1(unten links);2"
+  },
+  {
+    "name": "Abschleppseil f\u00fcr 3,5t, 5m lang",
+    "location": "G2(mitte links);1"
+  },
+  {
+    "name": "\u00d6lbinder in Kunststofffass",
+    "location": "G2(oben mitte);1"
+  },
+  {
+    "name": "Kraftstoffkanister TS",
+    "location": "G3(unten rechts, zw. Feuerl\u00f6scher und K\u00fcbelspritze)"
+  }
+]
+};
+// Initialize game state
+let items_left = [];
+var current_container_name;
+
+const next_stop_button_element = document.getElementById("next_stop_button");
+const itemElement = document.getElementById("item");
+// TIMIER !!!!!
+// initilize timer
+let timer_running = false;
+var timer_time_ms = 0;
+let intervalId = null;
+
+function updateTimer() {
+  let seconds = timer_time_ms / 1000; // divide by 10 to get 2 decimal places
+  let timerElement = document.getElementById('timer');
+  timerElement.textContent = `Gesammt Zeit ${seconds.toFixed(2)}s`; // display 2 decimal places
+}
+
+function startTimer() {
+  if (!timer_running) {
+    timer_running = true;
+    intervalId = setInterval(() => {
+      timer_time_ms+=100;
+      updateTimer();
+    }, 100); // update every 100ms to get 1 decimal place
+  }
+}
+
+function pauseTimer() {
+  if (timer_running) {
+    clearInterval(intervalId);
+    timer_running = false;
+  }
+}
+
+function unpauseTimer() {
+    startTimer();
+}
+
+function resetTimer() {
+  pauseTimer();
+  timer_time_ms = 0;
+  updateTimer();
+}
+
+// NAV OVERLAY
+function showNav() {
+  document.querySelector('.nav-icon').classList.add('active');
+  const navOverlay = document.querySelector('.nav-overlay');
+  navOverlay.classList.add('show');
+  navOverlay.style.animation = 'slideIn 0.5s forwards';
+}
+
+function hideNav() {
+  const navOverlay = document.querySelector('.nav-overlay');
+  if (navOverlay.classList.contains('show')) {
+    document.querySelector('.nav-icon').classList.remove('active');
+    navOverlay.classList.remove('show');
+    navOverlay.style.animation = 'slideOut 0.5s forwards';
+  }
+}
+
+// NAV OVERLAY
+function toggleNav() {
+  const navOverlay = document.querySelector('.nav-overlay');
+
+  if (navOverlay.classList.contains('show')) {
+    hideNav();
+  } else {
+    showNav();
+  }
+}
+
+
+const navOverlay = document.querySelector('.nav-overlay');
+
+let navOverlayButtons = [];
+
+function loadNavOverlay() {
+  Object.keys(item_containers).forEach((key) => {
+    const button = document.createElement('button');
+    button.id = key;
+    button.textContent = key;
+    button.onclick = function() {
+      startNewRound(this.id);
+    };
+    navOverlayButtons.push(button); // store the buttons in an array
+    navOverlay.appendChild(button);
+  });
+
+  updateHighlighting(); // call the updateHighlighting function initially to set the highlighting
+}
+
+function updateHighlighting() {
+  // remove the highlighted class from all buttons
+  navOverlayButtons.forEach((button) => {
+    button.classList.remove('highlighted');
+  });
+
+  // add the highlighted class to the button with the matching id
+  const highlightedButton = document.getElementById(current_container_name);
+  if (highlightedButton) {
+    highlightedButton.classList.add('highlighted');
+  }
+}
+
+function startNewRound(container_name){
+  loadContainer(container_name);
+  next_stop_button_element.innerHTML = `Neue Runde mit ${current_container_name} starten!`;
+  itemElement.innerHTML = "hier wird der Gesuchte gegenstand angezeigt"
+  updateHighlighting();
+  hideNav();
+  resetTimer();
+} 
+
+function loadContainer(container_name) {
+  items_left = shuffleList(item_containers[container_name]);
+  current_container_name = container_name;
+  update_progress_bar()
+}
+
+function shuffleList(list) {
+  // Create a copy of the original list to avoid modifying it
+  const shuffledList = [...list];
+  // Use the Fisher-Yates shuffle algorithm to shuffle the list
+  for (let i = shuffledList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+  }
+  return shuffledList;
+}
+
+function start_stop_timer() {
+  if (!timer_running) {
+    //next item
+    document.getElementById('location-hint').style.display = 'none';
+    if (items_left.length === 0) {
+      startNewRound(current_container_name)
+    } else {
+      show_next_item();
+      next_stop_button_element.innerHTML = "Gefunden!";
+      startTimer();
+    }
+  } else {
+    // item found
+    update_progress_bar()
+    pauseTimer();
+    if (items_left.length === 0){
+      show_results();
+    } else {
+      next_stop_button_element.innerHTML = "Nächster Gegenstand";
+    }
+  }
+}
+
+function show_results() {
+  itemElement.innerHTML = "Alle Gegenstände zugeordnet";
+  
+  const nextStopButtonElement = document.getElementById("next_stop_button");
+  nextStopButtonElement.innerHTML = `Erneute Runde mit ${current_container_name} Starten
+?</br>Alternativ:Oben rechts können andere Orte beübt werden.`;
+}
+
+function update_progress_bar(){
+  const progressBarElement = document.getElementById("progress-bar");
+  const progressTextElement = document.getElementById("progress-text");
+  const items_done = (item_containers[current_container_name].length -items_left.length)
+  const progress = (items_done / item_containers[current_container_name].length) * 100;
+  progressBarElement.style.width = `${progress}%`;
+  progressTextElement.textContent = `${items_done} von ${item_containers[current_container_name].length}`;
+}
+
+function show_next_item() {
+  let item = items_left.pop();
+  const itemElement = document.getElementById("item");
+  itemElement.innerHTML = item.name;
+  // Set location hint
+  const locationHintElement = document.getElementById("location-hint");
+  locationHintElement.innerHTML = item.location;
+
+}
+
+
+function showLocationHint() {
+  document.getElementById('location-hint').style.display = 'block';
+}
+
+//initial load
+//loadContainer("TSF-W");
+startNewRound("TSF-W");
+loadNavOverlay();
